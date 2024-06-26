f82524d8d3c472bb7b27b76a00ef70e2ac192bfb
Ulisses Batista Santana <santana.gru@gmail.com>
2024-06-25 21:37:10 -0300

    Validação com Bean Validation

--- src/main/java/com/tecmulti/bookmanagement/dto/BookDto.java
+++ src/main/java/com/tecmulti/bookmanagement/dto/BookDto.java
@@ -2,11 +2,22 @@ package com.tecmulti.bookmanagement.dto;
 
 import com.tecmulti.bookmanagement.entities.Book;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class BookDto {
 
 	private Long id;
+	
+	@Size(min = 3, max = 150, message = "title must be between 3 and 150 characters")
+	@NotBlank(message = "Required field" )
 	private String title;
+	
+	@Size(min = 3, max = 80, message = "title must be between 3 and 80 characters")
+	@NotBlank(message = "Required field" )
 	private String author;
+	
+	@Size(max = 13, message = "ISBN must contain 13 numbers" )
 	private String ISBN;
 	
 	
@@ -19,10 +30,27 @@ public class BookDto {
 	}
 	
 	public BookDto(Book entity) {
-		id = id;
-		title = title;
-		author = author;
-		ISBN = ISBN;
+		id = entity.getId();
+		title = entity.getTitle();
+		author = entity.getAuthor();
+		ISBN = entity.getISBN();
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+	
 	
 }
